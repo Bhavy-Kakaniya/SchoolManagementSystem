9cdb445f801ad0c2747e4289712b80df409ceaf5
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-04-27 15:50:05 +0530

    connecting Supabase PostgreSQL with Prisma

---
+++ backend/prisma.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { defineConfig } from "prisma/config";
+import { Pool } from 'pg';
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({
+	connectionString: process.env.DIRECT_URL
+})
+
+export default defineConfig({
+	schema: "prisma/schema.prisma",
+	datasource: { url: process.env.DATABASE_URL },
+	adapter: new PrismaPg(pool)
+});
