a78370945d90b4a5e1f3f15a7f39f5b5568dac19
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-04-30 18:37:59 +0530

    uploading schema to prisma and supabase

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,17 +1,13 @@
-import 'dotenv/config'
 import { defineConfig } from "prisma/config";
-import { Pool } from 'pg';
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const pool = new Pool({
-	connectionString: process.env.DIRECT_URL
-})
+import "dotenv/config"
 
 export default defineConfig({
-	schema: "prisma/schema.prisma",
-	migrations: {
-		path: "prisma/migration"
-	},
-	datasource: { url: process.env.DATABASE_URL },
-	adapter: new PrismaPg(pool)
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations"
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+	shadowDatabaseUrl: process.env.DIRECT_URL
+  }
 });
