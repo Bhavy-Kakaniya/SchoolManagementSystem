1298b071069c050cf2f872d70cc1e4233daf702e
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-07-28 23:17:36 +0530

    minor changes

--- frontend/src/app/student/page.tsx
+++ frontend/src/app/student/page.tsx
@@ -1,9 +1,23 @@
-export default function studentPage() {
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function StudentPage() {
+
+    const router = useRouter();
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken");
+        router.replace("/login");
+    }
+
     return (
         <>
             <div className="bg-red-400">
                 student page
             </div>
+            <button onClick={handleLogout} className="m-3 bg-blue-400 rounded p-2 cursor-pointer">Logout</button>
         </>
-    )
+    );
+    
+
 }
--- frontend/src/app/super-admin/page.tsx
+++ frontend/src/app/super-admin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation"
 
-export default function adminPage() {
+export default function SuperAdminPage() {
 
     const router = useRouter();
 
