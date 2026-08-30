71c030cebd4b9a26284a9b75781a5364c38525bb
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-30 23:22:04 +0530

    update controllers pass in catchAsync

--- backend/src/modules/student/student.controller.ts
+++ backend/src/modules/student/student.controller.ts
@@ -1,20 +1,21 @@
 import { Request, Response } from 'express';
 import { createStudentService, getStudentByIdService, getStudentsService } from './student.service';
 import { getStudentQuerySchema, studentIdParamSchema } from './student.validation';
+import catchAsync from '../../common/utils/catchAsync';
 
-export const createStudentController = async (req: Request, res: Response) => {
+export const createStudentController = catchAsync(async (req: Request, res: Response) => {
     const result = await createStudentService(req.user!.schoolId, req.body)
     res.status(201).json(result);
-}
+});
 
-export const getStudentController = async (req: Request, res: Response) => {
+export const getStudentController = catchAsync(async (req: Request, res: Response) => {
     const { page, limit } = getStudentQuerySchema.parse(req.query);
     const result = await getStudentsService(req.user!.schoolId, page, limit);
     res.json(result);
-}
+});
 
-export const getStudentByIdController = async (req: Request, res: Response) => {
+export const getStudentByIdController = catchAsync(async (req: Request, res: Response) => {
     const { id } = studentIdParamSchema.parse(req.params);
     const student = await getStudentByIdService(req.user!.schoolId, id);
     res.json(student);
-}
+});
