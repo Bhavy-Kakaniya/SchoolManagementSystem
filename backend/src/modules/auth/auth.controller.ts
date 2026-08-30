08346793e2510ae7d8d61808c379e7d2d39795a2
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-30 23:11:19 +0530

    update all controllers passing in catchAsync

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,36 +1,26 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { getMeService, loginUserService, logoutService, refreshTokenService } from "./auth.service";
 import catchAsync from "../../common/utils/catchAsync";
-import prisma from "../../config/prisma";
 
-export const loginController = catchAsync(
-    async (req: Request, res: Response) => {
-        const result = await loginUserService(req.body);
-        res.json(result);
-    }
-)
+export const loginController = catchAsync(async (req: Request, res: Response) => {
+    const result = await loginUserService(req.body);
+    res.json(result);
+});
 
-export const refreshTokenController = async (req: Request, res: Response) => {
+export const refreshTokenController = catchAsync(async (req: Request, res: Response) => {
     const result = await refreshTokenService();
     res.json(result);
-}
+});
 
-export const logoutController = async (req: Request, res: Response) => {
+export const logoutController = catchAsync(async (req: Request, res: Response) => {
     const result = await logoutService();
     res.json(result);
-}
+});
 
-export const getMeController = async (req: Request, res: Response) => {
-    const user = await prisma.user.findUnique({
-        where: { id: req.user?.userId },
-        include: { roles: { include: { role: true } } }
-    })
-
-    const rolesArray = user?.roles.map(userRole => userRole.role.name)
-
+export const getMeController = catchAsync(async (req: Request, res: Response) => {
+    const result = await getMeService(req.user!.userId);
     res.json({
         message: "Authorized user",
-        user: req.user,
-        rolesArray
+        ...result
     });
-}
+});
