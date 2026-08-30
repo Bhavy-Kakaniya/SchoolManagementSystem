11c58ba1de056bb573b74d22bdbebff9f3a22f6e
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-30 23:07:39 +0530

    update loginUserService and getMeService

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -5,16 +5,13 @@ import 'dotenv/config';
 import AppError from '../../errors/AppError';
 
 export const loginUserService = async (payload: { email: string, password: string }) => {
-
     // later also use school id for query so that find unique can be used and also it will
     //  allow 1 person working in more schools logic (tenant architecture).
     const user = await prisma.user.findFirst({ where: { email: payload.email } });
-
     if (!user)
-        throw new Error("User not found");
+        throw new AppError(401, "Invalid UserName or Password");
 
     const isPasswordMatched = await bcrypt.compare(payload.password, user.password);
-
     if (!isPasswordMatched)
         throw new AppError(401, "Invalid username or password")
 
@@ -40,22 +37,39 @@ export const loginUserService = async (payload: { email: string, password: strin
             email: user.email
         }
     };
-}
+};
+
+export const getMeService = async (userId: string) => {
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        include: { roles: { include: { role: true } } }
+    });
+
+    if(!user) {
+        throw new AppError(404, "User not found");
+    }
+
+    const rolesArray = user.roles.map(userRole => userRole.role.name);
+
+    return {
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            schoolId: user.schoolId
+        },
+        rolesArray
+    };
+};
 
 export const refreshTokenService = async () => {
     return {
         message: "refresh token service"
     }
-}
+};
 
 export const logoutService = async () => {
     return {
         message: "logout service"
     }
-}
-
-export const getMeService = async () => {
-    return {
-        message: "get me service"
-    }
-}
+};
