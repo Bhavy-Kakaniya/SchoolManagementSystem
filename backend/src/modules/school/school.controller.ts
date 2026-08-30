6e68e5523747ff9906eece446418a5f388125f57
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-08-30 23:25:09 +0530

    update controller pass in catchAsync

--- backend/src/modules/school/school.controller.ts
+++ backend/src/modules/school/school.controller.ts
@@ -1,9 +1,10 @@
+import catchAsync from "../../common/utils/catchAsync";
 import { createSchoolService } from "./school.service";
 import { createSchoolSchema } from "./school.validation"
 import { Request, Response } from "express";
 
-export const createSchoolController = async (req: Request, res: Response) => {
+export const createSchoolController = catchAsync(async (req: Request, res: Response) => {
     const validate = createSchoolSchema.parse(req.body);
     const result = await createSchoolService(validate);
     res.status(201).json(result);
-}
+});
