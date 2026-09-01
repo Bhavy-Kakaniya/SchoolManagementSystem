19f7bb02b89f5a67c4cd90c0c411cd68ca7c6996
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-09-01 23:13:36 +0530

    add student information page

---
+++ frontend/src/app/school-admin/students/[id]/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { getErrorMessage } from "@/lib/error";
+import { getStudentByIdService } from "@/services/student.service";
+import { Student } from "@/types/student";
+import { Button } from "@mui/material";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function StudentById() {
+    const { id } = useParams();
+    const router = useRouter();
+
+    const [student, setStudent] = useState<Student | null>(null);
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchStudent = async () => {
+            try {
+                if (!id) return;
+                const result = await getStudentByIdService(id.toString());
+                setStudent(result);
+            } catch (err) {
+                setError(getErrorMessage(err));
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchStudent();
+    }, [id]);
+
+    if (loading) return <p>Loading Student...</p>
+    if (error) return <p>{error}</p>
+    if (!student) return <p>Student not found</p>
+
+    return (
+        <div>
+            <h1>Student Details</h1>
+            <p>Name: {student.firstName} {student.lastName}</p>
+            <p>Admission No: {student.admissionNo}</p>
+            <p>Email: {student.user.email}</p>
+            <p>Gender: {student.gender}</p>
+            <p>Date of Birth: {student.dateOfBirth}</p>
+            <p>Phone: {student.phone || "Not provided"}</p>
+            <p>Blood Group: {student.bloodGroup || "Not provided"}</p>
+            <p>Address: {student.address || "Not provided"}</p>
+            <Button variant="contained" onClick={() => router.push(`/school-admin/students/${id}/edit`)}>Edit Student</Button>
+        </div>
+    );
+}
