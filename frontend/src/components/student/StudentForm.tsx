b12696478bc795e3a228c9e4424aa2654a3176d8
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-09-01 19:18:24 +0530

    update StudentForm introduce edit and create mode

--- frontend/src/components/student/StudentForm.tsx
+++ frontend/src/components/student/StudentForm.tsx
@@ -1,18 +1,42 @@
-"use client"
+"use client";
 
 import InputFieldTag from "@/components/InputField";
-import { CreateStudentValues } from "@/types/student";
+import {
+    CreateStudentValues,
+    Gender,
+    UpdateStudentValues
+} from "@/types/student";
 import { Button, MenuItem, TextField } from "@mui/material";
 import { useState } from "react";
 
 interface CreateStudentFormProps {
+    mode: "create";
     initialValues: CreateStudentValues;
     onSubmit: (values: CreateStudentValues) => void;
+}
+
+interface EditStudentFormProps {
+    mode: "edit";
+    initialValues: UpdateStudentValues;
+    onSubmit: (values: UpdateStudentValues) => void;
+}
+
+type StudentFormProps = | CreateStudentFormProps | EditStudentFormProps;
+
+type StudentFormState = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: Gender;
+    dateOfBirth: string;
+    phone: string;
+    bloodGroup: string;
+    address: string;
+    admissionNo?: string;
 };
 
-export default function CreateStudentForm({ initialValues, onSubmit }: CreateStudentFormProps) {
-    const [formValues, setFormValues] = useState<CreateStudentValues>(initialValues);
-
+export default function StudentForm(props: StudentFormProps) {
+    const [formValues, setFormValues] = useState<StudentFormState>(props.initialValues);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
         setFormValues((previous) => ({
@@ -21,9 +45,16 @@ export default function CreateStudentForm({ initialValues, onSubmit }: CreateStu
         }));
     };
 
-    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        onSubmit(formValues);
+        if (props.mode === "create") {
+            props.onSubmit({
+                ...formValues,
+                admissionNo: formValues.admissionNo!
+            });
+        } else {
+            props.onSubmit(formValues);
+        }
     };
 
     return (
@@ -31,17 +62,20 @@ export default function CreateStudentForm({ initialValues, onSubmit }: CreateStu
             <InputFieldTag name="firstName" label="First Name" value={formValues.firstName} onChange={handleChange} required />
             <InputFieldTag name="lastName" label="Last Name" value={formValues.lastName} onChange={handleChange} required />
             <InputFieldTag name="email" label="Email" value={formValues.email} type="email" onChange={handleChange} required />
-            <InputFieldTag name="admissionNo" label="Admission Number" value={formValues.admissionNo} onChange={handleChange} required />
+            {props.mode === "create" && (
+                <InputFieldTag name="admissionNo" label="Admission Number" value={formValues.admissionNo ?? ""} onChange={handleChange} required />
+            )}
             <TextField select name="gender" label="Gender" value={formValues.gender} onChange={handleChange} required margin="normal" sx={{ width: "100%" }}>
-                <MenuItem value="MALE">Male</MenuItem>
-                <MenuItem value="FEMALE">Female</MenuItem>
-                <MenuItem value="OTHER">Other</MenuItem>
+                <MenuItem value={Gender.MALE}>Male</MenuItem>
+                <MenuItem value={Gender.FEMALE}>Female</MenuItem>
+                <MenuItem value={Gender.OTHER}>Other</MenuItem>
             </TextField>
+
             <InputFieldTag name="dateOfBirth" label="Date Of Birth" type="date" value={formValues.dateOfBirth} onChange={handleChange} required />
             <InputFieldTag name="phone" label="Phone" value={formValues.phone} onChange={handleChange} />
             <InputFieldTag name="bloodGroup" label="Blood Group" value={formValues.bloodGroup} onChange={handleChange} />
             <InputFieldTag name="address" label="Address" value={formValues.address} onChange={handleChange} />
-            <Button type="submit" variant="contained">Create Student</Button>
+            <Button type="submit" variant="contained">{props.mode === "create" ? "Create Student" : "Update Student"}</Button>
         </form>
     );
-};
+}
