0c19f4a076fdf77af91b89e77cd3466b4b4ec6e7
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-09-02 11:32:12 +0530

    add deactivate student route

--- backend/src/modules/student/student.route.ts
+++ backend/src/modules/student/student.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import authMiddleware from '../../common/middleware/auth.middleware';
 import { requireRoles } from '../../common/middleware/authorization.middleware';
 import { RoleName } from "@prisma/client";
-import { createStudentController, getStudentByIdController, getStudentController, updateStudentController } from "./student.controller";
+import { createStudentController, deactivateStudentController, getStudentByIdController, getStudentController, updateStudentController } from "./student.controller";
+
 const router = Router();
 
 router.post('/', authMiddleware, requireRoles(RoleName.ADMIN), createStudentController);
 router.get('/', authMiddleware, requireRoles(RoleName.ADMIN, RoleName.PRINCIPAL, RoleName.TEACHER), getStudentController);
-router.get('/:id', authMiddleware, requireRoles(RoleName.ADMIN, RoleName.PRINCIPAL, RoleName.TEACHER), getStudentByIdController)
-router.put("/:id", authMiddleware, requireRoles(RoleName.ADMIN), updateStudentController)
+router.get('/:id', authMiddleware, requireRoles(RoleName.ADMIN, RoleName.PRINCIPAL, RoleName.TEACHER), getStudentByIdController);
+router.put("/:id", authMiddleware, requireRoles(RoleName.ADMIN), updateStudentController);
+router.patch("/:id/deactivate", authMiddleware, requireRoles(RoleName.ADMIN), deactivateStudentController);
 
 export default router;
